2e127f7dd3b186d2d788e2f0cff61d476872f37c
Ernesto Ojeda <ernesto.ojeda@intel.com>
2024-03-12 10:23:54 -0700

    feat: new top n filter feature and handle pre-tournament display

--- pga.py
+++ pga.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import requests
 import time
-import os
+from argparse import ArgumentParser
 from bs4 import BeautifulSoup
 from rich.live import Live
 from rich.table import Table
+
 
 def get_player_data():
     """Get player data from ESPN"""
@@ -12,6 +13,8 @@ def get_player_data():
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
     html = requests.get("https://www.espn.com/golf/leaderboard", headers=headers).text
     soup = BeautifulSoup(html, features="html.parser")
+
+    tournament_title = soup.find('h1', ['Leaderboard__Event__Title']).text
 
     cutline_row = soup.find_all('tr', ['cutline'])
     cutline = None
@@ -24,14 +27,25 @@ def get_player_data():
 
     rows = soup.find_all('tr', ['PlayerRow__Overview'])
     lowest_score_today = 0
-    
+
+    index = 0
     for row in rows:
         data = list(row.children)
-        standing = data[1].text
-        player = data[3].text
-        score = data[4].text
-        today = data[5].text
-        thru = data[6].text
+        # tournament has not started
+        if len(data) == 3:
+            standing = f'{index}' # <-- this is not not technically accurate since they are all tied, but allows for easy filtering
+            player = data[1].text
+            score = 'E'
+            today = None
+            thru = data[2].text
+            t_round = 0
+        elif len(data) > 5:
+            standing = data[1].text
+            player = data[3].text
+            score = data[4].text
+            today = data[5].text
+            thru = data[6].text
+            t_round = 1
 
         try:
             today_score = int(today)
@@ -46,13 +60,16 @@ def get_player_data():
             if score_num > cutline:
                 is_cut = True
 
-        players.append({ "player": player, "standing": standing, "score": score, "today": today, "today_score": today_score, "thru": thru, "hot": False, "is_cut": is_cut })
+        players.append({"tournament": tournament_title, "player": player, "standing": standing, "score": score,
+                       "today": today, "today_score": today_score, "thru": thru, "hot": False, "is_cut": is_cut, "round": t_round })
+
+        index = index + 1
 
     for player in players:
         if player['today_score'] is not None:
             if (player['today_score'] == lowest_score_today) or (player['today_score'] - lowest_score_today < 2):
                 player['hot'] = True
-    
+
     return players
 
 
@@ -70,55 +87,86 @@ def get_score_as_int(score_txt):
     return final_score
 
 
-def generate_table() -> Table:
+def generate_table(args) -> Table:
     """Make a new table."""
-    table = Table(width=os.get_terminal_size().columns)
-    table.add_column("⛳", justify='center')
-    table.add_column("Player")
-    table.add_column("Thru", justify="right")
-
     players = get_player_data()
+
+    first_player = players[0]
+
+    table = Table(expand=True, highlight=True)
+    table.add_column("⛳", justify='center')
+    table.add_column(first_player['tournament'], overflow='ellipsis')
+    table.add_column('Thru' if first_player['round'] > 0 else 'Tee Time', justify='right')
+
     cutline_added = False
     for player in players:
-        if player['is_cut'] and not cutline_added:
+        standing = player['standing']
+        if standing is not None:
+            standing = int(standing.replace('T', ''))
+
+        if (standing is None and args.top is None) or standing <= args.top:
+            if player['is_cut'] and not cutline_added:
+                table.add_row(
+                    '', '✂️  -----------------------', ''
+                )
+                cutline_added = True
+
+            score_color = 'white'
+            if '-' in player['score']:
+                score_color = 'red'
+            elif player['score'] == 'E':
+                score_color = 'green'
+
+            player_name = player['player']
+            if player['hot']:
+                player_name = f"{player_name} 🔥"
+                try:
+                    thru_hole = int(player['thru'])
+                    leader_thru = int(players[0]['thru'])
+                    if thru_hole < leader_thru:
+                        player_name = f"[u]{player_name}[/u]"
+                except:
+                    pass
+
+            if player['thru'] == 'F':
+                player_name = f"[rgb(150,150,150)]{player_name}"
+
+            if player['is_cut']:
+                player_name = f"[strike]{player_name}[/strike]"
+
+            player_text = f"{player_name}"
+            if player['today'] is not None:
+                player_text += f" ({player['today']})"
+
             table.add_row(
-                "", "✂️  -----------------------", ""
+                f"[{score_color}]{player['score']}", player_text, player['thru']
             )
-            cutline_added = True
-
-        score_color = 'white'
-        if '-' in player['score']:
-            score_color = 'red'
-        elif player['score'] == 'E':
-            score_color = 'green'
-
-        player_name = player['player']
-        if player['hot']:
-            player_name = f"{player_name} 🔥"
-            try:
-                thru_hole = int(player['thru'])
-                leader_thru = int(players[0]['thru'])
-                if thru_hole < leader_thru:
-                    player_name = f"[u]{player_name}[/u]"
-            except:
-                pass
-
-        if player['thru'] == 'F':
-            player_name = f"[rgb(150,150,150)]{player_name}"
-
-        if player['is_cut']:
-            player_name = f"[strike]{player_name}[/strike]"
-
-        table.add_row(
-            f"[{score_color}]{player['score']}", f"{player_name} ({player['today']})", player['thru']
-        )
     return table
 
+
+def get_parser():
+    """ setup parser and return parsed command line arguments
+    """
+    parser = ArgumentParser(
+        description='PGA leaderboard from ESPN now in your terminal')
+    parser.add_argument(
+        '--top',
+        type=int,
+        required=False,
+        default=1000,
+        help='Top [n] players to show')
+
+    return parser
+
+
 def main():
-    with Live(generate_table(), refresh_per_second=1) as live:
+    parser = get_parser()
+    args = parser.parse_args()
+
+    with Live(generate_table(args), refresh_per_second=1) as live:
         while True:
             time.sleep(30)
-            live.update(generate_table())
+            live.update(generate_table(args))
 
 
 if __name__ == '__main__':
